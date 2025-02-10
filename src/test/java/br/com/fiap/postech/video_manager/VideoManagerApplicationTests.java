f52934bdfc258a0984c38d8db7fa2eed64846772
raylanef <raylanefernandes@hotmail.com>
2025-02-09 22:34:05 -0300

    ajuste teste

--- src/test/java/br/com/fiap/postech/video_manager/VideoManagerApplicationTests.java
+++ src/test/java/br/com/fiap/postech/video_manager/VideoManagerApplicationTests.java
@@ -3,7 +3,6 @@ package br.com.fiap.postech.video_manager;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
-@SpringBootTest
 class VideoManagerApplicationTests {
 
 	@Test
