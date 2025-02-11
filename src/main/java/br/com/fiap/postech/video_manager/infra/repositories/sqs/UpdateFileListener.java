0ee34a3673b38112ee1c1e0b9b3a0d5afcd74e56
John Gomes <silva.johngomes@gmail.com>
2025-02-10 23:58:35 -0300

    log

--- src/main/java/br/com/fiap/postech/video_manager/infra/repositories/sqs/UpdateFileListener.java
+++ src/main/java/br/com/fiap/postech/video_manager/infra/repositories/sqs/UpdateFileListener.java
@@ -2,14 +2,16 @@ package br.com.fiap.postech.video_manager.infra.repositories.sqs;
 
 
 import io.awspring.cloud.sqs.annotation.SqsListener;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.messaging.handler.annotation.Payload;
 import org.springframework.stereotype.Component;
 
+@Slf4j
 @Component
 public class UpdateFileListener {
 
     @SqsListener("${aws.sqs.queue-url}")
     public void listen(@Payload String message) {
-        System.out.println("Mensagem recebida do SQS: " + message);
+        log.info("Mensagem recebida do SQS: {}", message);
     }
 }
