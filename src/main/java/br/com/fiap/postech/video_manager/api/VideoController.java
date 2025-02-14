2bde565583401efa55e838e16b15b75c95efc4d6
John Gomes <silva.johngomes@gmail.com>
2025-02-13 23:07:30 -0300

    configs

--- src/main/java/br/com/fiap/postech/video_manager/api/VideoController.java
+++ src/main/java/br/com/fiap/postech/video_manager/api/VideoController.java
@@ -5,9 +5,11 @@ import br.com.fiap.postech.video_manager.api.dtos.UploadResponseDto;
 import br.com.fiap.postech.video_manager.domain.entities.UploadModel;
 import br.com.fiap.postech.video_manager.domain.entities.VideoUploadRequest;
 
+import br.com.fiap.postech.video_manager.domain.usecases.DownloadFileUseCase;
 import br.com.fiap.postech.video_manager.domain.usecases.FindUploadUseCase;
 import br.com.fiap.postech.video_manager.domain.usecases.UploadFileUseCase;
 
+import lombok.RequiredArgsConstructor;
 import org.springframework.data.domain.Page;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
@@ -15,25 +17,17 @@ import org.springframework.web.multipart.MultipartFile;
 
 
 
+@RequiredArgsConstructor
 @RestController
 @RequestMapping("/videos")
 public class VideoController {
 
     private final UploadFileUseCase uploadFileUseCase;
-
-    public VideoController(UploadFileUseCase uploadFileUseCase) {
-        this.uploadFileUseCase = uploadFileUseCase;
-    }
-
-    @GetMapping
-    public ResponseEntity<Page<UploadResponseDto>> findAll() {
-        return  ResponseEntity.ok().build();
-    }
+    private final DownloadFileUseCase downloadFileUseCase;
 
     @GetMapping("/{id}/frames")
     public ResponseEntity<byte[]> downloadFramesById(@PathVariable("id") String uploadId) {
-
-        return ResponseEntity.ok("seu-video.zip".getBytes());
+        return ResponseEntity.ok(downloadFileUseCase.execute(uploadId));
     }
 
     @PostMapping
--- src/main/java/br/com/fiap/postech/video_manager/domain/usecases/DownloadFileUseCase.java
+++ src/main/java/br/com/fiap/postech/video_manager/domain/usecases/DownloadFileUseCase.java
@@ -1,4 +1,17 @@
 package br.com.fiap.postech.video_manager.domain.usecases;
 
+import br.com.fiap.postech.video_manager.infra.repositories.bucket.FileService;
+import br.com.fiap.postech.video_manager.infra.repositories.sqs.models.S3Object;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Component;
+
+@RequiredArgsConstructor
+@Component
 public class DownloadFileUseCase {
+
+    private final FileService fileService;
+
+    public byte[] execute(String fileName) {
+        return fileService.getFileFromS3(fileName);
+    }
 }
